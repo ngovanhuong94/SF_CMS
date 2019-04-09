8f03dec42affa29856d5e5154da288af1c3eaadb
ngovanhuong94 <ngovanhuong21994@gmail.com>
2019-04-09 20:14:10 +0300

    all files

--- wsgi.py
+++ wsgi.py
@@ -2,8 +2,12 @@ import os
 from dotenv import load_dotenv
 
 dotenv_path = os.path.join(os.path.dirname(__file__), '.env')
+flaskenv_path = os.path.join(os.path.dirname(__file__), '.flaskenv')
 if os.path.exists(dotenv_path):
     load_dotenv(dotenv_path)
+
+if os.path.exists(flaskenv_path):
+    load_dotenv(flaskenv_path)
 
 from blog import create_app
 
